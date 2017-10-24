426a1df63ad2335ed22fc9c67df33907f0187885
jmoostland <jeanineoostland@gmail.com>
2017-10-24 21:05:09 +0200

    tweede commit

--- index.php
+++ index.php
@@ -8,6 +8,25 @@ $nummer = array ("1","2","3","4");
           echo "<br>";
           echo $nummer[$lift];
           echo $naamverdieping [$lift];
+          
         }
-
+          echo "<br>";
 ?>
+
+<?php
+    echo "<br>";
+    echo "<b> Gebouw 4 </b>";
+    echo "<br>";
+    echo "<br>";
+    
+$naamverdiepingen = array("1 receptie","2 horeca", "3 vergaderruimte", "4 kantoren.");
+$arrlenght = count ($naamverdiepingen);
+
+    echo "Dit gebouw bestaat uit de volgende verdiepingen:";
+    echo "<br>";
+        for($x = 0;$x < $arrlenght; $x++){  
+            echo $naamverdiepingen[$x];
+            echo "<br>";
+        }
+?>
+
